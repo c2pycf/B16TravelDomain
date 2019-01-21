194c8cb02c37a9669dd4cf8797762631fea59dd4
Fang Chen <c2pycf@users.noreply.github.com>
2019-01-20 22:42:25 -0600

    Update TicketDetailActivity

---
+++ app/src/main/java/com/example/fang/b16traveldomain/ticketConfirmation/TicketDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.fang.b16traveldomain.ticketConfirmation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fang.b16traveldomain.R;
+
+public class TicketDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_detail);
+    }
+}
