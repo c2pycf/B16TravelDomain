ce948b552ceb7d4d94db12c8a72344409df34b67
Fang Chen <c2pycf@users.noreply.github.com>
2019-01-20 23:01:44 -0600

    Testing branch

--- app/src/main/java/com/example/fang/b16traveldomain/ticketConfirmation/TicketDetailActivity.java
+++ app/src/main/java/com/example/fang/b16traveldomain/ticketConfirmation/TicketDetailActivity.java
@@ -7,12 +7,14 @@ import com.example.fang.b16traveldomain.R;
 
 public class TicketDetailActivity extends AppCompatActivity {
     static private String TAG = TicketDetailActivity.class.getSimpleName();
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_detail);
+
+        
 
 
     }
