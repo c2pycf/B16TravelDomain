eb4f90c46fddb78a2d6e876c3cf2ae8108ee7e0e
Fang Chen <c2pycf@users.noreply.github.com>
2019-01-22 22:45:12 -0600

    Update payment activity -- Fang

--- app/src/main/java/com/example/fang/b16traveldomain/paymentgateway/PaymentContract.java
+++ app/src/main/java/com/example/fang/b16traveldomain/paymentgateway/PaymentContract.java
@@ -1,9 +1,20 @@
 package com.example.fang.b16traveldomain.paymentgateway;
 
+import com.example.fang.b16traveldomain.model.TicketInformation;
+import com.stripe.android.model.Card;
+
 public interface PaymentContract {
     interface PaymentView{
-        void createCardToken(Card card);
+        void createCard();
+
+        void showToast(String msg);
+
+        void showConfirmedActivity();
     }
 
-    interface PaymentPresenter{}
+    interface PaymentPresenter{
+        void proceedPayment(Card card, TicketInformation ticketInformation);
+
+        void proceedOrder(TicketInformation ticketInformation);
+    }
 }
